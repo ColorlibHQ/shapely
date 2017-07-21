bfe0ae8286ec90758c73ba0e9ae9d192f319d5f0
Giucu <giucu91@yahoo.com>
2017-07-21 13:35:40 +0300

    Remove duplicate row.

--- template-parts/layouts/blog-large-image.php
+++ template-parts/layouts/blog-large-image.php
@@ -8,8 +8,8 @@ while ( have_posts() ) : the_post();
 	 * If you want to override this in a child theme, then include a file
 	 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 	 */
-	?> <div class="row"> <?php
-	get_template_part( 'template-parts/content', 'grid-wide' );
-	?> </div> <?php
+	?> 
+	<?php get_template_part( 'template-parts/content', 'grid-wide' ); ?>
+<?php
 endwhile;
 
