71ebeaff1ee2129e457fccfa081dbff14fde112e
Giucu <giucu91@yahoo.com>
2017-07-21 11:56:48 +0300

    Fixed 404 page

--- 404.php
+++ 404.php
@@ -22,36 +22,6 @@ get_header(); ?>
 
 					<?php
 						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-
-						// Only show the widget if site has multiple categories.
-					if ( shapely_categorized_blog() ) :
-					?>
-
-					<div class="widget widget_categories">
-					<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'shapely' ); ?></h2>
-					<ul>
-					<?php
-					wp_list_categories( array(
-						'orderby'    => 'count',
-						'order'      => 'DESC',
-						'show_count' => 1,
-						'title_li'   => '',
-						'number'     => 10,
-					) );
-				?>
-				</ul>
-				</div><!-- .widget -->
-
-				<?php
-						endif;
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'shapely' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
 					?>
 
 				</div><!-- .page-content -->
