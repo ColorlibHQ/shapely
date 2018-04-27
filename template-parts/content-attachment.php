34bfd02366d28740d69627418d31cb621eda76fa
cristianraiber <cristian@machothemes.com>
2018-04-27 18:11:00 +0300

    wrong function used

--- template-parts/content-attachment.php
+++ template-parts/content-attachment.php
@@ -31,7 +31,7 @@
 		$image = wp_get_attachment_image( get_the_ID(), 'full' );
 
 		echo wp_kses_post( $image );
-		echo the_content();
+		echo get_the_content();
 
 		$link_pages_args = array(
 			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'shapely' ),
